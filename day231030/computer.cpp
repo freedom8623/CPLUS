b8014e8fc0c5727b7db872ae99ef3adfba14c8fd
freedom8623 <862319379@qq.com>
2023-10-31 08:50:04 +0800

    电脑组装案例完成

--- day231030/computer.cpp
+++ day231030/computer.cpp
@@ -61,5 +61,58 @@ class LenovoMemoryBank :public Absmemorybank{
     public:
     void storage(){cout<<"Lenovo MemoryBank is working"<<endl;};
 };
+//电脑类
+class Computer{
+    public:
+    Computer(string *desc,AbsCpu *Cpu,AbsGraphicsCard *Gcard,Absmemorybank *Abank):m_desc(desc),m_Cpu(Cpu),m_Gcard(Gcard),m_Mbank(Abank){}
+    ~Computer()
+    {
+        
+        if(m_desc != NULL){delete m_desc;m_desc = NULL;}
+        if(m_Cpu != NULL){ delete m_Cpu;m_Cpu = NULL;}
+        if(m_Gcard !=NULL){ delete m_Gcard;m_Gcard = NULL;}
+        if(m_Mbank != NULL){delete m_Mbank;m_Mbank = NULL; }
+    }
 
+    //制作流程
+    void Makeup()
+    {
+        cout<<setw(25)<<setfill("*")<<"开始组装"<<*m_desc<<setw(15)<<""<<endl;
+        m_Cpu->calculate();
+        m_Gcard->show();
+        m_Mbank->storage();
+         cout<<setw(30)<<setfill("*")<<"组装完成"<<*m_desc<<setw(20)<<""<<endl;
+    }
+
+    private:
+    string *m_desc;
+    AbsCpu *m_Cpu;
+    AbsGraphicsCard *m_Gcard;
+    Absmemorybank *m_Mbank;
+};
+
+void MakeComputer()
+{
+    //第一台电脑的零件
+    AbsCpu* IntelCpu = new IntelCpu;
+    AbsGraphicsCard* IntelGraphicscard = new IntelGraphicscard;
+    Absmemorybank* IntelMemoryBank = new IntelMemoryBank;
+
+    //创建第一台电脑
+    string* Computer_name1 = new strint("Intel电脑");
+    Computer* Computer1 = new Computer1(Computer1,IntelCpu,IntelGraphicscard,IntelMemoryBank);
+    Computer1->Makeup();
+    delete Computer1;
+
+    //第一台电脑的零件
+    AbsCpu* IntelCpu = new LenovoCpu;
+    AbsGraphicsCard* LenovoGraphicsCard = new LenovoGraphicsCard;
+    Absmemorybank* LenovoMemoryBank = new LenovoMemoryBank;
+
+    //创建第二台电脑
+    string* Computer_name2 = new string("Lenovo电脑");
+    Computer* Computer2 = new Computer2(Computer2,LenovoCpu,LenovoGraphicsCard,LenovoMemoryBank);
+
+
+}
 
